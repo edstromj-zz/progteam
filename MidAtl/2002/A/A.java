d8ac6b172c88804a4534adbf44b95b0296dbe1e0
John Edstrom <jedstrom@nearinfinity.com>
2012-10-04 21:24:53 -0400

    Fixed indenting in 2002 files

--- MidAtl/2002/A/A.java
+++ MidAtl/2002/A/A.java
@@ -2,19 +2,19 @@ import java.util.*;
 import java.text.*;
 
 public class A {
-	public static void main(String[] args) {
-		new A().solve();
-	}
-	public void solve() {
-		Scanner in = new Scanner(System.in);
-		DecimalFormat df = new DecimalFormat("0.00");
-		double start = Double.parseDouble(in.next());
-		while (true) {
-			double curr = Double.parseDouble(in.next());
-			if (curr == 999.0) break;
-			System.out.println(df.format(curr-start));
-			start = curr;
-		}
-		System.out.println("End of Output");
-	}
+  public static void main(String[] args) {
+    new A().solve();
+  }
+  public void solve() {
+    Scanner in = new Scanner(System.in);
+    DecimalFormat df = new DecimalFormat("0.00");
+    double start = Double.parseDouble(in.next());
+    while (true) {
+      double curr = Double.parseDouble(in.next());
+      if (curr == 999.0) break;
+      System.out.println(df.format(curr-start));
+      start = curr;
+    }
+    System.out.println("End of Output");
+  }
 }
--- MidAtl/2002/G/G.java
+++ MidAtl/2002/G/G.java
@@ -1,31 +1,31 @@
 import java.util.*;
 
 public class G {
-	public static void main(String [] args) {
-		new G().solve();
-	}
-	public void solve() {
-		Scanner in = new Scanner(System.in);
-		while(in.hasNext()) {
-			int o = Integer.parseInt(in.next());
-			int sz = (int)Math.pow(3, o);
-			char [] line = new char[sz];
-			for (int i = 0 ; i < sz ; i++)
-				line[i] = '-';
-			cantor(line, 0, sz);
-			System.out.println(new String(line));
-		}
-	}
-	public void cantor(char [] line, int start, int end) {
-		if ((end - start) == 1) return;
-		int interval = (end - start)/3;
-		clear(line, start+interval, start+2*interval);
-		cantor(line, start, start+interval);
-		cantor(line, start+2*interval, end);
-	}
-	public void clear(char [] line, int start, int end) {
-		for (int i = start; i < end ; i++) {
-			line[i] = ' ';
-		}
-	}
+  public static void main(String [] args) {
+    new G().solve();
+  }
+  public void solve() {
+    Scanner in = new Scanner(System.in);
+    while(in.hasNext()) {
+      int o = Integer.parseInt(in.next());
+      int sz = (int)Math.pow(3, o);
+      char [] line = new char[sz];
+      for (int i = 0 ; i < sz ; i++)
+        line[i] = '-';
+      cantor(line, 0, sz);
+      System.out.println(new String(line));
+    }
+  }
+  public void cantor(char [] line, int start, int end) {
+    if ((end - start) == 1) return;
+    int interval = (end - start)/3;
+    clear(line, start+interval, start+2*interval);
+    cantor(line, start, start+interval);
+    cantor(line, start+2*interval, end);
+  }
+  public void clear(char [] line, int start, int end) {
+    for (int i = start; i < end ; i++) {
+      line[i] = ' ';
+    }
+  }
 }
